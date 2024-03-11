613a2739ccc72dde65a037ab499ef1742f185d8b
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-03-11 22:27:11 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,7 @@
+//Write a java program to display your name and title in two diffrent lines.
+class name{
+	public static void main(String a[]){
+		System.out.println("Subham");
+		System.out.println("Shaw");
+	}
+}
