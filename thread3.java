5e0f58d9f6409175beb8b5ebae02f67747c0b14c
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-03-14 22:33:39 +0530

    Add files via upload

---
+++ thread3.java
@@ -0,0 +1,27 @@
+class Thread1 implements Runnable
+{
+Thread1()
+{
+Thread t=new Thread();
+t.start();
+}
+public void run()
+{
+for(int i=0;i<10;i++)
+{
+System.out.println("Thread 1's i="+i);
+}
+}
+}
+class Thread2
+{
+public static void main(String args[])
+{
+new Thread1();
+for(int i=0;i<=20;i++)
+{
+System.out.println("Main i="+i);
+}
+}
+}
+
