78447b36b5c8a67ea1b63c057baeb7d72d0bdcae
Subham Shaw <subhamkrshawa21s@gmail.com>
2024-07-31 19:32:12 +0530

    Add files via upload

---
+++ evod.java
@@ -0,0 +1,14 @@
+class evod{
+public static void main(String args[]){
+int num=4;
+if(num%2==0){
+System.out.println("The number is even");
+}
+else{
+System.out.println("The number is odd");	
+}
+}
+}
+
+
+
